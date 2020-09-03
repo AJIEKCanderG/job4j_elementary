f370b5ca860c0f250e3f236ce63f56b92bde2ea8
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-09-04 01:13:42 +0300

    3.3. Разбор первой программы. [#353214]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Grigorev Aleksandr");
+            System.out.println("21.11.1986");
+}
+}
