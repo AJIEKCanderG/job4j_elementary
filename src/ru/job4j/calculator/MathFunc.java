383c1d0f3c25bf7913c73dcb4b3d048618612315
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-09-07 23:25:36 +0300

    Конвертер валюты [#353194]

---
+++ src/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+
+    public static int func1(int x) {
+        return x*2+1;
+    }
+
+    public static void main(String[] args) {
+        int result1 = MathFunc.func1(100);
+        System.out.println(result1);
+    }
+}
---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(120);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("120 rubles are " + dollar + " dollar.");
+    }
+}
