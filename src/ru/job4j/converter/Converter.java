7d20f75ebac9d9920384e10f7447d7583dc0d50d
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-09-09 16:30:19 +0300

    Модульные тесты. [#353227]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,10 +3,12 @@ package ru.job4j.converter;
 public class Converter {
 
     public static int rubleToEuro(int value) {
+
         return value / 70;
     }
 
     public static int rubleToDollar(int value) {
+
         return value / 60;
     }
 
