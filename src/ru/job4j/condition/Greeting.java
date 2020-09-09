af25ed6a71000c72b5740b582693fafdaada2c8c
grigoriev <sanya.spb.vohta@gmail.com>
2020-09-09 10:00:44 +0300

    String [#353205]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,9 +4,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        String id = "I like Java!" + "But I am a newbie.";
+        idea += "But I am a newbie.";
         int year = 2020;
-        idea = id + year;
+        idea += year;
         System.out.println(idea);
     }
 }
