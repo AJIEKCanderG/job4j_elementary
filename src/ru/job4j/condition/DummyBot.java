048fee908be37da4c86d58c609cb39c361440706
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-09-11 17:39:52 +0300

    Глупый бот [#353140]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
+            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+}
