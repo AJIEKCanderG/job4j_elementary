ea38dba672c2162962a2a1187fad9dd65eb31bca
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-09-13 16:26:28 +0300

    Оператор && [#353150]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,18 +1,18 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is : " + age);
-        if (age >= 18) {
-            System.out.println("Welcome to the cinema.");
-        } else  {
-            System.out.println("It is not for you.");
+        public static void permission(boolean allowByParent, boolean hasMoney) {
+            if (allowByParent && hasMoney) {
+                System.out.println("I can go to the cinema.");
+            } else {
+                System.out.println("I can't.");
+            }
+        }
+
+        public static void main(String[] args) {
+            permission(true, true);
+            permission(true, false);
+            permission(false, true);
+            permission(false, false);
         }
     }
-
-    public static void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
-
-    }
-}
