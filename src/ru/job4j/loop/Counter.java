ffe60542d500fab257402f0b18264fbdd2692d6f
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-09-15 16:44:16 +0300

    5.0.1. Сумма чисел. [#353256]

---
+++ src/ru/job4j/loop/Counter.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+    int sum = 0;
+    for (int x = start; x <= finish; x++) {
+        sum = sum + x;
+    }
+    return sum;
+}
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(2, 7));
+        System.out.println(sum(7, 8));
+    }
+}
