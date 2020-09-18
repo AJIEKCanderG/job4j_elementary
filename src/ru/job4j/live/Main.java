b388be4aba2ed72a8dc3f9450630c9fcfadee6d9
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-09-18 10:34:58 +0300

    5.9.1. Генерация конструкций в IDEA [#353258]

---
+++ src/ru/job4j/live/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
