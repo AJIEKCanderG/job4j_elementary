0739adcbcd75891ee1de51c85211f098e28d0d8d
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-09-22 22:29:57 +0300

    6.4. Слово начинается с ... [#353165]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -5,8 +5,10 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
-            return word[index + 1] == pref[index + 1];
+            if (pref[index] != word[index]) {
+                return false;
+            }
         }
-        return true;
+        return result;
     }
 }
