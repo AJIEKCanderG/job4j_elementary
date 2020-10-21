3868989de3b038d40324a1afd636c72102b7c295
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-21 14:21:18 +0300

    Задача 20. Вывести четные в порядке убывания индексов

--- src/ru/job4j/array/EvenPrint.java
+++ src/ru/job4j/array/EvenPrint.java
@@ -3,9 +3,18 @@ package ru.job4j.array;
 public class EvenPrint {
     public static void print(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                System.out.println(array[array.length - i] + "");
+            if (array[array.length - 1 - i] % 2 == 0) {
+                System.out.println(array[array.length - 1 - i] + "");
             }
         }
     }
 }
+
+//Второй вариант решения:
+    /* public static void print(int[] array) {
+     for (int i = array.length - 1; i >= 0; i--) {
+         if (array[i] % 2 == 0) {
+             System.out.println(array[i]  + "");
+         }
+     }
+*/
--- test/ru/job4j/array/EvenPrintTest.java
+++ test/ru/job4j/array/EvenPrintTest.java
@@ -16,9 +16,7 @@ public class EvenPrintTest {
         System.setOut(new PrintStream(out));
         EvenPrint.print(new int[] {1, 2, 3, 4});
         String ln = System.lineSeparator();
-        assertThat(out.toString(), is(
-                "4" + ln + "2" + ln
-        ));
+        assertThat(out.toString(), is("4" + ln + "2" + ln));
     }
 
     @Test
@@ -27,6 +25,15 @@ public class EvenPrintTest {
         System.setOut(new PrintStream(out));
         EvenPrint.print(new int[] {1, 3, 5});
         assertThat(out.toString(), is(""));
+    }
+
+    @Test
+    public void whenNonEmpty2() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        EvenPrint.print(new int[] {2, 4, 6, 8});
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("8" + ln + "6" + ln + "4" + ln + "2" + ln));
     }
 
 }
