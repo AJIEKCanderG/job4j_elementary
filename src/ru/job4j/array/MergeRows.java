e889a05934befa7ce6ee2cde3215305fdbd0e38b
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-22 13:20:42 +0300

    Задача 25. Объединить строки квадратного массива

---
+++ src/ru/job4j/array/MergeRows.java
@@ -0,0 +1,47 @@
+package ru.job4j.array;
+
+public class MergeRows {
+    public static int[] merge(int[][] data) {
+        int[] array = new int[data.length * data.length];
+        int index = 0;
+        for (int[] datum : data) {
+            for (int i : datum) {
+                array[index] = i;
+                index++;
+            }
+        }
+        return array;
+    }
+}
+
+//  Второе решение после подсказки про foreache:
+// (выше решение уже с применением forache)
+/*
+    public static int[] merge(int[][] data) {
+        int[] array = new int[data.length * data.length];
+        int index = 0;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++)  {
+                array[index] = data[i][j];
+                index++;
+            }
+        }
+        return array;
+    }
+}*/
+
+
+// Первое решение
+/*
+        int[] array = new int[data.length * data.length];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                int temp = data[i][j];
+                data[i][j] = array[i * data.length + j];
+                array[i * data.length + j] = temp;
+            }
+
+        }
+        return array;
+    }
+}*/
---
+++ test/ru/job4j/array/MergeRowsTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeRowsTest {
+
+    @Test
+    public void test() {
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] out = MergeRows.merge(input);
+        assertArrayEquals(expected, out);
+    }
+
+    @Test
+    public void test2() {
+        int[][] input = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+        int[] expected = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] out = MergeRows.merge(input);
+        assertArrayEquals(expected, out);
+    }
+
+    @Test
+    public void test3() {
+        int[][] input = {
+                {16, 15, 14, 13},
+                {12, 11, 10, 9},
+                {8, 7, 6, 5},
+                {4, 3, 2, 1}
+        };
+        int[] expected = {16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] out = MergeRows.merge(input);
+        assertArrayEquals(expected, out);
+    }
+
+    @Test
+    public void test4() {
+        int[][] input = {
+                {10, 9, 8, 7},
+                {6, 5, 4},
+                {3, 2},
+                {1}
+        };
+        int[] expected = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] out = MergeRows.merge(input);
+        assertArrayEquals(expected, out);
+    }
+
+}
