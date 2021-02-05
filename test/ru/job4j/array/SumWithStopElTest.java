9b1c0317040c4706c9a81f1742f663684a8b66b1
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-02-05 20:47:32 +0300

    Упражнения.1.3.1. Коллекция List, ArrayList. 10. Удаление элементов по условию

---
+++ test/ru/job4j/array/SumWithStopElTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.core.Is.is;
+
+public class SumWithStopElTest {
+    @Test
+    public void countNotEven() {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int rsl = SumWithStopEl.count(data, 7);
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void countNotEven2() {
+        int[] data = {2, 1, 3, 8, 5, 6, 7, 8, 9, 10};
+        int rsl = SumWithStopEl.count(data, 5);
+        assertThat(rsl, is(14));
+    }
+
+    @Test
+    public void countEven() {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int rsl = SumWithStopEl.count(data, 5);
+        assertThat(rsl, is(10));
+    }
+}
