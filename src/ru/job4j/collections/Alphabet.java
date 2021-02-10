39c1eb92d531cf00bfb886e13470e37139048a19
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-10 18:15:17 +0300

    Упражнения.1.3.1. Коллекция List, ArrayList. 16. Буквы в алфавитном порядке.

---
+++ src/ru/job4j/collections/Alphabet.java
@@ -0,0 +1,26 @@
+package ru.job4j.collections;
+
+import java.util.*;
+
+/** 16. Буквы в алфавитном порядке.
+ * Вам необходимо реализовать метод, который принимает строку, которое является одним словом, без знаков препинания цифр и пробелов.
+ * Метод должен вернуть новую строку, в которой все символы исходной строки будут расставлены в алфавитном порядке.
+ * Для разделения строки на подстроки используйте метод split(), добавьте все элементы в коллекцию List<String>,
+ * отсортируйте в порядке возрастания - используйте метод sort, в который передадите компаратор. Новую строку сформируйте с помощью StringBuilder.*/
+
+public class Alphabet {
+    public static String reformat(String s) {
+        String[] s1 = s.split("");
+        List<String> rsl = new ArrayList<>();
+        Collections.addAll(rsl, s1);
+        Collections.sort(rsl);
+        StringBuilder newS = new StringBuilder();
+        newS.append(char.rsl);
+        return newS.toString();
+    }
+}
+
+
+
+
+
