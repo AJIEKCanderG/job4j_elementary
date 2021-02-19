409674dfc162df4b25708fb7c8f6b3e163db0a2b
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-19 13:13:49 +0300

    Упражнения  1.3.2. Коллекция Set, HashSet  1. Уникальные имена

---
+++ test/ru/job4j/collections/UniqueNameTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static ru.job4j.collections.UniqueName.User;
+
+public class UniqueNameTest {
+    @Test
+    public void collectUniqueName() {
+        List<User> users = List.of(
+                new User(1, "name1"),
+                new User(2, "name2"),
+                new User(3, "name1"),
+                new User(4, "name3"),
+                new User(5, "name5"),
+                new User(6, "name3")
+        );
+        int rsl = UniqueName.collectUniqueName(users);
+        int expected = 4;
+        assertThat(rsl, is(expected));
+    }
+}
