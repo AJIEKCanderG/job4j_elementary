54604d9b797b34ea28a5a9a86a977afa60aab668
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-19 14:16:33 +0300

    Упражнения 1.3.2. Коллекция Set, HashSet 5. Джекпот в казино

---
+++ src/ru/job4j/collections/Jackpot.java
@@ -0,0 +1,18 @@
+package ru.job4j.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Jackpot {
+    public static boolean checkYourWin(String[] combination) {
+        Set<String> set = new HashSet<>();
+        for (String word : combination) {
+            set.add(word);
+            if (set.size() != 1) {
+                return false;
+            }
+        }
+
+    return true;
+}
+}
