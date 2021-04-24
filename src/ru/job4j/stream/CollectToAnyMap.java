8a750dcbc563fb2e56fe70b78d09b80077d5d090
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-04-25 00:41:51 +0300

    Упражнения 1.4.2. Stream API 2.6. Сборка элементов. Сборка в произвольную реализацию Map

---
+++ src/ru/job4j/stream/CollectToAnyMap.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 2.6. Сборка элементов. Сборка в произвольную реализацию Map
+ * Ранее мы сталкивались с реализацией toMap с тремя аргументами. Этот метод перегружен и имеет реализацию с четырьмя аргументами,
+ * где в качестве четвертого аргумента мы можем указать реализацию Map, которую мы хотим получить
+ * Например
+ * Map<Integer, Integer> map = List.of(1, 2, 1).stream().collect(Collectors.toMap(k -> k, v -> v, (prev, curr) -> prev, TreeMap::new));
+ * В данном случае мы получим реализацию Map, но только TreeMap.
+ * Ваша задача собрать Map из стрима чисел, где ключ это само число, а значение его квадрат. Сборка должна быть в LinkedHashMap
+ */
+
+public class CollectToAnyMap {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toMap(k -> k, k -> k * k, (prev, curr) -> prev, LinkedHashMap::new));
+    }
+}
+
