c82dd2e8468874bf81a430f071136918ae2f3581
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-04-25 00:52:59 +0300

    Упражнения 1.4.2. Stream API 3. Терминальные и промежуточные операции

---
+++ src/ru/job4j/stream/ForEachMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * 4.1. Терминальные операции. Метод forEach()
+ * Если нам нужно выполнить какое-то действие над каждым элементом стрима, при этом нам не нужно ничего возвращать, то мы можем использовать метод forEach().
+ * Например,
+ * List.of(1, 2, 3).stream().forEach(n -> System.out.println(n));
+ * Ваша задача вывести элементы стрима в консоль через System.out.println()
+ */
+
+public class ForEachMethod {
+    public static void show(Stream<Integer> data) {
+        data.forEach(System.out::println);
+    }
+}
