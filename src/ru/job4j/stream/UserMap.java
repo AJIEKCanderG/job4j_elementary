7b5f9cc90bb5de475f952a9344edb71f6de5c40a
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-04-25 16:25:49 +0300

    Упражнения 1.4.2. Stream API 5.3. Промежуточные операции. Метод map(). Map с объектами

---
+++ src/ru/job4j/stream/UserMap.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 5.3. Промежуточные операции. Метод map(). Map с объектами
+ * Дан список возрастов.
+ * Вам нужно создать список пользователей с такими возрастами
+ */
+
+public class UserMap {
+
+    public static class User {
+
+        private int age;
+
+        public User(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
+    public static List<User> map(List<Integer> ages) {
+        return ages.stream().map(User::new).collect(Collectors.toList());
+    }
+}
