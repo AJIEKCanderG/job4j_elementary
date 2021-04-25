798ec018393c0d9a0d6a35ccce104a7fb96e5fdb
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-04-25 19:31:55 +0300

    Упражнения 1.4.2. Stream API 5.8. Промежуточные операции. Метод flatMap()

---
+++ src/ru/job4j/stream/FlatMapForObject.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 5.8. Промежуточные операции. Метод flatMap()
+ * Метод flatMap() служит для получения стрима объектов, т.е. Stream.
+ * Например,
+ * List<List<Integer>> lists = List.of(List.of(1, 2), List.of(3, 4));
+ * list.stream() // получаем стрим из List<Integer>
+ * .flatMap(subList -> subList.stream()) // получаем стрим из элементов
+ * .forEarch(el -> System.out.println(el))
+ * Ваша задача элементы вложенных списков собрать в один список
+ */
+
+public class FlatMapForObject {
+    public static List<Integer> flatMap(List<List<Integer>> lists) {
+        return lists.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+}
