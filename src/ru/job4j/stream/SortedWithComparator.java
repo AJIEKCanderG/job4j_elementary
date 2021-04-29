ccb4c499d883fc1e60dcb92d9e939544a7efc0b5
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-04-29 23:52:05 +0300

    Упражнения 1.4.2. Stream API 9.2. Промежуточные операции. Метод sorted() с компаратором.

---
+++ src/ru/job4j/stream/SortedWithComparator.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 9.2. Промежуточные операции. Метод sorted() с компаратором.
+ * Метод sorted() может принимать также Comparator.
+ * Ваша задача отсортировать строки по убыванию.
+ * Нужно использовать Comparator.reverseOrder() для получения компаратора
+ */
+
+public class SortedWithComparator {
+    public static List<String> sorted(List<String> strings) {
+        return strings.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    }
+}
