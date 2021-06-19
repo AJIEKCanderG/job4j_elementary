7264c33eb569269f8a5465e63d66878e603f25de
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-19 23:55:26 +0300

    Упражнения 1.1.5. Циклы 7. Найти степень числа

---
+++ src/ru/job4j/loop/DegreeLoop.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+/**
+ * 7. Найти степень числа
+ * Дано целое число a и целое число degree (> 0).
+ * Найти a в степени degree: a^degree = a·a· . . . ·a (числа a перемножаются deg
+ */
+
+public class DegreeLoop {
+    public static int calculate(int a, int n) {
+        int sum = 1;
+        for (int i = 0; i < n; i++) {
+            sum = sum * a;
+
+        }
+        return sum;
+    }
+}
