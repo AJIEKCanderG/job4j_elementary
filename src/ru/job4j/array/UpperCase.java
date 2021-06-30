9e521b358ed4b1cbe6cb0b8bcd84d0c6c2b33dde
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-30 14:46:11 +0300

    Упражнения 1.4.1. Lambda 2.2. Fuction. Получить корень квадратный из числа

--- src/ru/job4j/array/UpperCase.java
+++ src/ru/job4j/array/UpperCase.java
@@ -9,8 +9,8 @@ package ru.job4j.array;
 public class UpperCase {
     public static char[] toUpperCase(char[] string) {
         char[] str = new char[string.length];
-        for(int i =0; i < string.length; i++) {
-            if(Character.isLetter(string[i])) {
+        for (int i = 0; i < string.length; i++) {
+            if (Character.isLetter(string[i])) {
                 str[i] = Character.toUpperCase(string[i]);
             } else {
                 str[i] = string[i];
---
+++ src/ru/job4j/lambda/FunctionSqrt.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.function.Function;
+
+/**
+ * 2.2. Fuction. Получить корень квадратный из числа
+ * Ниже приведен код. Вам нужно дописать лямбда выражение, которое извлекает корень квадратный из числа.
+ */
+
+public class FunctionSqrt {
+    public static double calculate(double x) {
+        return calculate(Math::sqrt, x);  //(y -> Math.sqrt(y), x);
+    }
+
+    public static double calculate(Function<Double, Double> y, double x) {
+        return y.apply(x);
+    }
+}
