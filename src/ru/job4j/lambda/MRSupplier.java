b99863ed7997feb49f8a29d1950691e999a38978
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-30 15:12:40 +0300

    Упражнения 1.4.1. Lambda 4.4. Method reference и Supplier

---
+++ src/ru/job4j/lambda/MRSupplier.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * 4.4. Method reference и Supplier
+ * Интерфейс Supplier служит для того, чтобы обернуть логику создания объекта.
+ * Он ничего не принимает, только возвращает объект, который ему поручили создать.
+ * Ссылки на методы поддерживает метод new, который позволяет нам создать нужный нам объект
+ *
+ * Например,
+ * Supplier<List<String>> supplier = ArrayList::new;
+ * List<String> list = supplier.get() // вернет ArrayList
+ * Ваша задача вернуть Supplier, который создает пустую строку через new
+ */
+
+public class MRSupplier {
+    public static Supplier<String> supplier() {
+       return String::new;
+    }
+}
