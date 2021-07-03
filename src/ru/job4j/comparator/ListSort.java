c91b86c80a34e2d16346abcc5d71044896616146
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-03 17:14:17 +0300

    Упражнения 1.3.4. Сортировка 2.3. Способы сортировки. Сортировка List

---
+++ src/ru/job4j/comparator/ListSort.java
@@ -0,0 +1,21 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 2.3. Способы сортировки. Сортировка List
+ * Класс List имеет метод sort(), который позволяет отсортировать элементы списка.
+ * Например:
+ * List<Integer> data = Arrays.asList(3, 2, 1);
+ * data.sort(Comparator.naturalOrder()); // Получим список из элементов 1, 2, 3
+ * Ваша задача отсортировать список строк по убыванию. В списке могут быть null элементы,
+ * они должны оказаться в конце после сортировки.
+ */
+
+public class ListSort {
+    public static List<String> sort(List<String> data) {
+        data.sort(Comparator.nullsLast(Comparator.reverseOrder()));
+        return data;
+    }
+}
