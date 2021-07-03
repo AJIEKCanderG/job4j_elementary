ca1a5d7dbe9501172d48f3b8070378fea377f7cd
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-03 17:04:59 +0300

    Упражнения 1.3.4. Сортировка 2.2. Способы сортировки. Сортировка массива объектов

---
+++ src/ru/job4j/comparator/ArraysSortWithObject.java
@@ -0,0 +1,21 @@
+package ru.job4j.comparator;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 2.2. Способы сортировки. Сортировка массива объектов
+ * В утилитном классе Arrays есть метод для сортировки массива объектов void sort(массив, компаратор)
+ * Например:
+ * Integer[] data = {3, 2, 1};
+ * Arrays.sort(data, Comparator.naturalOrder()); // теперь массив станет {1, 2, 3}
+ * Ваша задача отсортировать массив строк в убывающем порядке и вернуть его. Среди строк нет null элементов.
+ */
+
+public class ArraysSortWithObject {
+    public static String[] sort(String[] data) {
+        Arrays.sort(data, Comparator.reverseOrder());
+        return data;
+    }
+}
+
