48027661552dfde3ef6802389c56f95be6873b2c
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-14 23:53:25 +0300

    Упражнения 1.1.6. Массивы 2.9. Привести строку к верхнему регистру, убрав лишние символы, цифры и пробелы  Обновление задачи

--- test/ru/job4j/array/UpperCaseTest.java
+++ test/ru/job4j/array/UpperCaseTest.java
@@ -8,23 +8,30 @@ import static org.junit.Assert.*;
 public class UpperCaseTest {
 
     @Test
-    public void test() {
-        char[] input = "alpha-123".toCharArray();
-        char[] expect = "ALPHA-123".toCharArray();
+    public void LettersLowerCaseWithDigits() {
+        char[] input = "alpha012".toCharArray();
+        char[] expect = "ALPHA".toCharArray();
         assertArrayEquals(expect, UpperCase.toUpperCase(input));
     }
 
     @Test
-    public void test3() {
-        char[] input = "al1/ha-1-2-3".toCharArray();
-        char[] expect = "AL1/HA-1-2-3".toCharArray();
+    public void LettersLowerCaseWithDigitsWithSymbols() {
+        char[] input = "alpha-12@?-".toCharArray();
+        char[] expect = "ALPHA".toCharArray();
         assertArrayEquals(expect, UpperCase.toUpperCase(input));
     }
 
     @Test
-    public void test2() {
-        char[] input = "a123b".toCharArray();
-        char[] expect = "A123B".toCharArray();
+    public void LettersLowerCaseAndOneUpperCaseDigitsSymbolsSpace() {
+        char[] input = "aLpha - 1-0!@#$5".toCharArray();
+        char[] expect = "APHA".toCharArray();
+        assertArrayEquals(expect, UpperCase.toUpperCase(input));
+    }
+
+    @Test
+    public void LettersLowerCaseAndTwoUpperCaseDigitsSpace() {
+        char[] input = "aLpHa - 1".toCharArray();
+        char[] expect = "APA".toCharArray();
         assertArrayEquals(expect, UpperCase.toUpperCase(input));
     }
 
