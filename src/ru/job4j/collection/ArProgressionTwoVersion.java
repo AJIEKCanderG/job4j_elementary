c986c8825a998419e6cc077cca8ff924395d511e
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-22 14:41:08 +0300

    refactor

--- src/ru/job4j/collection/ArProgressionTwoVersion.java
+++ src/ru/job4j/collection/ArProgressionTwoVersion.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class ArProgressionTwoVersion {
     public static int checkData(List<Integer> data) {
-        int sum = data.get(0);
+        int sum = data.get(0) + data.get(data.size() - 1);
         for (int i = 1; i < data.size() - 1; i++) {
             if (data.get(i) != (data.get(i - 1) + data.get(i + 1)) / 2) {
                 return 0;
             }
-            sum = sum + data.get(i);
+            sum += data.get(i);
         }
-        return sum + data.get(data.size() - 1);
+        return sum;
     }
 }
