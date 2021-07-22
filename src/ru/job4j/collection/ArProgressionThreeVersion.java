ab66620c08ed88c3cd18d57ba0df6482fd2bd9dc
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-22 16:28:19 +0300

    refactor

--- src/ru/job4j/collection/ArProgressionThreeVersion.java
+++ src/ru/job4j/collection/ArProgressionThreeVersion.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class ArProgressionThreeVersion {
     public static int checkData(List<Integer> data) {
-        int sum = data.get(0) + data.get(1);
+        int sum = 0;
         for (int i = 2; i < data.size(); i++) {
             if (data.get(i - 1) == (data.get(i - 2) + data.get(i)) / 2) {
-                sum += data.get(i);
+                sum = data.stream().mapToInt(x -> x).sum();
             } else {
                 sum = 0;
                 break;
