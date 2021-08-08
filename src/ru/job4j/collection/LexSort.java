ca2ba6841b02215908176a951334618bdd07f76f
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-08-08 22:29:39 +0300

    refactor sort

--- src/ru/job4j/collection/LexSort.java
+++ src/ru/job4j/collection/LexSort.java
@@ -6,10 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-       String[] l1 = left.split("\\.");
-       String[] r1 = right.split("\\.");
-       int a = Integer.parseInt(l1[0]);
-       int b = Integer.parseInt(r1[0]);
-       return Integer.compare(a, b);
+        String[] l1 = left.split("\\.");
+        String[] r1 = right.split("\\.");
+        return Integer.compare(Integer.parseInt(l1[0]), Integer.parseInt(r1[0]));
     }
 }
