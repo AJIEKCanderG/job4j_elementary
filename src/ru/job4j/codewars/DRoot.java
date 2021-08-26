949ff62ab18863c7f8a95f23c22a4108d87d1916
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-08-27 02:35:35 +0300

    codewars - Sum of Digits / Digital Root

--- src/ru/job4j/codewars/DRoot.java
+++ src/ru/job4j/codewars/DRoot.java
@@ -1,5 +1,15 @@
 package ru.job4j.codewars;
 
+/**
+ * Digital root is the recursive sum of all the digits in a number.
+ * Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing
+ * in this way until a single-digit number is produced. The input will be a non-negative integer.
+ * Examples
+ *     16  -->  1 + 6 = 7
+ *    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+ * 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+ */
+
 public class DRoot {
     public static int digital_root(int n) {
         int rsl = 0;
