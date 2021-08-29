3b4bfd424edf699359c66da806917aa409bac6c6
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-08-29 16:29:18 +0300

    codewars Dashatize it

---
+++ src/ru/job4j/codewars/DashatizeIt.java
@@ -0,0 +1,20 @@
+package ru.job4j.codewars;
+
+/**  Dashatize it
+ * Given a variable n,
+ * If n is an integer, Return a string with dash'-'marks before and after each odd integer,
+ * but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
+ * If n is not an integer, return an empty value.
+ * Ex:
+ * dashatize(274) -> '2-7-4'
+ * dashatize(6815) -> '68-1-5'
+ */
+public class DashatizeIt {
+    public static String dashatize(int num) {
+        return Integer.toString(num)
+                .replaceAll("([13579])","-$1-")
+                .replaceAll("--","-")
+                .replaceAll("^-","")
+                .replaceAll("-$","");
+    }
+}
---
+++ test/ru/job4j/codewars/DashatizeItTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DashatizeItTest {
+
+
+        @Test
+        public void testBasic() {
+            assertEquals("2-7-4", DashatizeIt.dashatize(274));
+            assertEquals("5-3-1-1", DashatizeIt.dashatize(5311));
+            assertEquals("86-3-20", DashatizeIt.dashatize(86320));
+            assertEquals("9-7-4-3-02", DashatizeIt.dashatize(974302));
+        }
+
+        @Test
+        public void testWeird() {
+            assertEquals("0", DashatizeIt.dashatize(0));
+            assertEquals("1", DashatizeIt.dashatize(-1));
+            assertEquals("28-3-6-9", DashatizeIt.dashatize(-28369));
+        }
+
+        @Test
+        public void testEdgeCases() {
+            assertEquals("2-1-4-7-48-3-64-7", DashatizeIt.dashatize(Integer.MAX_VALUE));
+            assertEquals("2-1-4-7-48-3-648", DashatizeIt.dashatize(Integer.MIN_VALUE));
+            assertEquals("1-1-1-1-1-1-1-1-1-1", DashatizeIt.dashatize(-1111111111));
+        }
+
+    }
