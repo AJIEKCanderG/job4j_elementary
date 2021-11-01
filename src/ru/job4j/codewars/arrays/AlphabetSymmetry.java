aa83ab71ac2326ed9e2174b25a39f1d76ea1cb02
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-11-01 19:45:22 +0300

    codewars

---
+++ src/ru/job4j/codewars/arrays/AlphabetSymmetry.java
@@ -0,0 +1,27 @@
+package ru.job4j.codewars.arrays;
+
+/**  7 kyu   Alphabet symmetry
+ * Учитывая массив слов, верните массив количества букв,
+ * занимающих свои позиции в алфавите для каждого слова. Например,
+ * solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+ * @author Ajiekcander
+ */
+public class AlphabetSymmetry {
+    public static int[] solve(String[] arr){
+        String s = "abcdefghijklmnopqrstuvwxyz";
+        int[] in = new int[arr.length];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].toLowerCase();
+            System.out.println(arr[i]);
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (j == s.indexOf(arr[i].charAt(j))) {
+                    count++;
+                }
+            }
+            in[i] = count;
+            count = 0;
+        }
+        return in;
+    }
+}
---
+++ test/ru/job4j/codewars/arrays/AlphabetSymmetryTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.codewars.arrays;
+
+import org.junit.Test;
+import java.util.Random;
+import static org.junit.Assert.assertArrayEquals;
+
+public class AlphabetSymmetryTest {
+
+    private static final String a = "abcdefghijklmnopqrstuvwxyz";
+    private static final Random random = new Random();
+    private static int[] mySolve(String[] arr){
+        int[] s = new int[arr.length];
+        int c, j, t;
+        for(int i=0;i<arr.length;i++){
+            t = Math.min(arr[i].length(), 26);
+            c = 0;
+            for(j=0;j<t;j++){
+                if(Character.toLowerCase(arr[i].charAt(j))==a.charAt(j))
+                    c++;
+            }
+            s[i] = c;
+        }
+        return s;
+    }
+    private static int random(int min, int max){
+        return random.nextInt(max-min)+min;
+    }
+
+    @Test
+    public void sampleTest(){
+        assertArrayEquals(new int[]{4,3,1},AlphabetSymmetry.solve(new String[]{"abode","ABc","xyzD"}));
+        assertArrayEquals(new int[]{4,3,0},AlphabetSymmetry.solve(new String[]{"abide","ABc","xyz"}));
+        assertArrayEquals(new int[]{6,5,7},AlphabetSymmetry.solve(new String[]{"IAMDEFANDJKL","thedefgh","xyzDEFghijabc"}));
+        assertArrayEquals(new int[]{1,3,1,3},AlphabetSymmetry.solve(new String[]{"encode","abc","xyzD","ABmD"}));
+    }
+    @Test
+    public void randomTest(){
+        final String upper = a.toUpperCase();
+        String[] L;
+        int j, k, r, r0, r1, r2, d;
+        StringBuilder s;
+        String s1;
+        for(int i=0;i<100;i++){
+            r = random(500,1000);
+            L = new String[r-2];
+            for(j=2;j<r;j++){
+                r0 = random(0,10);
+                r1 = random(0,10);
+                r2 = random(r1,26);
+                s = new StringBuilder();
+                for(k=0;k<r0;k++)
+                    s.append(a.charAt(random(0, 26)));
+                s1 = upper.substring(0,r1) + s.toString().toUpperCase();
+                d =  r1-r0;
+                if(d>=0)
+                    s1 += a.substring(d,r2);
+                L[j-2] = s1;
+            }
+            assertArrayEquals(mySolve(L),AlphabetSymmetry.solve(L));
+        }
+    }
+}
