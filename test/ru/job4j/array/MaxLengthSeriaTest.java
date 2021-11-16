59e21b468cc707868e85f4761d7d6841690d3f44
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-11-16 16:02:27 +0300

    update test

--- test/ru/job4j/array/MaxLengthSeriaTest.java
+++ test/ru/job4j/array/MaxLengthSeriaTest.java
@@ -22,4 +22,9 @@ public class MaxLengthSeriaTest {
         assertEquals(9, MaxLengthSeria.find(new int[] {6, 7, 8, 9, 10, 11, 1, 11, 12, 13, 14, 15, 16, 17, 18, 2, 3, 5, 2, 6, 3}));
     }
 
+    @Test
+    public void testTwoEquals() {
+        assertEquals(5, MaxLengthSeria.find(new int[] {6, 7, 8, 9, 10, 7, 2, 3, 4, 5, 6, 3}));
+    }
+
 }
